55cad17f48e878b79be8befa51706e520bd43376
sam9525 <sam9985246327@gmail.com>
2025-04-17 16:36:18 +0930

    1448 JAVA Ver DFS

---
+++ Leetcode 1448 - count-good-nodes-in-binary-tree/1448.count-good-nodes-in-binary-tree.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=1448 lang=java
+ *
+ * [1448] Count Good Nodes in Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+  public int goodNodes(TreeNode root) {
+    return dfs(root, root.val);
+  }
+
+  public int dfs(TreeNode root, int maxVal) {
+    if (root == null) return 0;
+
+    int res = (root.val > maxVal) ? 1 : 0;
+    maxVal = Math.max(root.val, maxVal);
+    res += dfs(root.left, maxVal);
+    res += dfs(root.right, maxVal);
+
+    return res;
+  }
+}
+// @lc code=end
